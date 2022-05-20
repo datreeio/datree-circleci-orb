aae91b6d52564099d77b5aa89d51b86b692566b5
Naor Peled <thehecticbyte@gmail.com>
2022-05-20 23:33:10 +0300

    feat: add orb logic

---
+++ src/scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+targetPath="$TARGET_PATH"
+arguments="$ARGUMENTS"
+isHelmChart="$IS_HELM_CHART"
+helmArguments="$HELM_ARGUMENTS"
+isKustomization="$IS_KUSTOMIZATION"
+kustomizeArguments="$KUSTOMIZE_ARGUMENTS"
+
+printf "datree version: "
+datree version
+printf "\n"
+
+if [ -z "$DATREE_TOKEN" ]; then
+    printf "No account token configured, see https://github.com/datreeio/action-datree for instructions\n"
+    exit 1
+fi
+
+if [ "$isHelmChart" = "true" ]; then
+    helm datree test $targetPath $arguments -- $helmArguments
+elif [ "$isKustomization" = "true" ]; then
+    datree kustomize test $targetPath $arguments -- $kustomizeArguments
+else
+    datree test $targetPath $arguments
+fi
