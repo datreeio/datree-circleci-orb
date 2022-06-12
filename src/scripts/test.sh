5e299e52c1337a48f036a463aa9a522ce5b05d9f
Naor Peled <me@naor.dev>
2022-06-12 15:30:40 +0300

    added scripts

---
+++ src/scripts/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+file="$FILE"
+cliArguments="$ARGUMENTS"
+isHelmChart="$IS_HELM_CHART"
+helmArguments="$HELM_ARGUMENTS"
+isKustomization="$IS_KUSTOMIZATION"
+kustomizeArguments="$KUSTOMIZE_ARGUMENTS"
+
+printf "datree version: "
+datree version
+printf "\n"
+
+if [ -z "$DATREE_TOKEN" ]; then
+    printf "No account token configured, see https://github.com/datreeio/action-datree for instructions\n"
+    exit 1
+fi
+
+if [ "$isHelmChart" = "true" ]; then
+    printf "Running datree on helm chart\n"
+    helm datree test $file $cliArguments -- $helmArguments
+elif [ "$isKustomization" = "true" ]; then
+    printf "Running datree on kustomization\n"
+    datree kustomize test $file $cliArguments -- $kustomizeArguments
+else
+    printf "Running datree on file\n"
+    datree test $file $cliArguments
+fi
